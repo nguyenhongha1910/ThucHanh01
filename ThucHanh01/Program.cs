e6a4aec33f686a073827f5dc24aaec304ef0f97e
hoan47 <b2h1604bmt@gmail.com>
2024-01-25 11:05:10 +0700

    Anh Hoan da thay doi

--- ThucHanh01/Program.cs
+++ ThucHanh01/Program.cs
@@ -18,7 +18,6 @@ namespace ThucHanh1
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-            return;
         }
     }
 }
